8956ff367db638f2fc43f7b1ea5c858b9eb7cdae
Brandon U <unzueta.brandon@gmai.com>
2019-06-19 21:31:32 -0700

    my jquery

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -33,3 +33,20 @@ $.ajax ({
     }
 
 })
+var submitIsClicked = false; 
+function clickHandler(){ 
+  submitIsClicked = true;
+  console.log(submitIsClicked);
+}
+
+var element = document.getElementById('submitSearch'); // grab a reference to your element
+element.addEventListener('click', clickHandler); // associate
+
+var clearIsClicked = false; 
+function clickClearHandler(){ 
+  clearIsClicked = true;
+  console.log(clearIsClicked);
+}
+
+var element = document.getElementById('clearSearch'); // grab a reference to your element
+element.addEventListener('click', clickClearHandler); // associate
